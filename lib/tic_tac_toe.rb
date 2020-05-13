d4326c4e40878742ba0cae541c35a80665b2d0b1
Jung Lee <lee.jung.s95@gmail.com>
2020-05-13 03:37:51 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -57,7 +57,7 @@ class TicTacToe
   end
 
   def won?
-    TicTacToe::WIN_COMBIANTIONS.each do |win_combo|
+    WIN_COMBIANTIONS.each do |win_combo|
       win_index_1 = win_combo[0]
       win_index_2 = win_combo[1]
       win_index_3 = win_combo[2]
