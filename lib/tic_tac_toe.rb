77af39451f210a17e54df6c4940f9f7b54b289a2
Jung Lee <lee.jung.s95@gmail.com>
2020-05-13 03:36:09 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -65,6 +65,9 @@ class TicTacToe
       position_1 = @board[win_index_1]
       position_2 = @board[win_index_2]
       position_3 = @board[win_index_3]
+
+      if position_1 == "X" && position_2 == "X" && position_3 == "X"
+        return win_combo
   end
 
   def turn_count
